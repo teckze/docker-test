ec807884c9eb5412db63fc9e0186b968dcfb692e
afreensaba97@gmail.com <144366724+teckze@users.noreply.github.com>
2025-05-26 22:09:50 +0530

    demo-app

--- src/main/java/com/psa/controller/HelloWorldController.java
+++ src/main/java/com/psa/controller/HelloWorldController.java
@@ -1,10 +1,11 @@
 package com.psa.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	
+	@GetMapping("/message")
 	public String getMessage() {
 		return "hello";
 	}
